9a2eee4343ba923c97f526ac98d6d6bdc5275a43
Mahe123 <mahender.wa309@gmail.com>
2016-07-30 22:16:50 -0600

    Added the new files

---
+++ AutoMaTellerMachine/AutoMaTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoMaTellerMachine.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        [Route("Home/About")]
+        public ActionResult Index()
+        {
+            return View("CarousalTemplate");
+        }
+        
+        public ActionResult About()
+        {
+            ViewBag.MessageAbout = "Your application description page.";
+
+            return View();
+        }
+        
+        public ActionResult Contact()
+         {
+            ViewBag.MessageContact = "Having Trouble ? send us a Message";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            ViewBag.MessageContact = "Thanks,we got Your Message!";
+
+            return View();
+        }
+
+        public ActionResult Serial(String letterCase)
+        {
+            var Serial = "ASPNETMVCATM1";
+            if (letterCase == "lower")
+            {
+                return Content(Serial.ToLower());
+            }
+            return Json(new { name = "Serial", Value = Serial }, JsonRequestBehavior.AllowGet);
+        }
+           }
+}
